238bb4c2ec92653b7739318798fd70195b761d66
Shane Reilly <shanereilly@protonmail.com>
2024-08-03 06:57:45 -0400

    problem 5

--- python/problem005.py
+++ python/problem005.py
@@ -1,23 +1,6 @@
 from math import lcm
-
-def reduce_list(ns: list[int]) -> list[int]:
-    if 0 in ns:
-        ns.remove(0)
-    if 1 in ns:
-        ns.remove(1)
-    reduced_list : list[int] = ns.copy()
-    for i in ns:
-        for j in ns:
-            if i * j in ns:
-                if i in reduced_list:
-                    reduced_list.remove(i)
-                if j in reduced_list:
-                    reduced_list.remove(j)
-    return reduced_list
-
 def calculate() -> str:
-    ns : list[int] = reduce_list(list(range(2,21)))
-    result = lcm(*ns)
+    result = lcm(*range(2,21))
     return str(result)
 
 if __name__ == "__main__":
